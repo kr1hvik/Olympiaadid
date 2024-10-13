948d7d8c9149d0e3b0e90fa3481994ea5e0cae76
Pärtel <partel.kukk@gmail.com>
2024-10-13 18:00:55 +0300

    vandra doesent work yet

---
+++ eio2024-lv/vandraTestJah.py
@@ -0,0 +1,12 @@
+import sys
+
+if sys.stdin.readline().strip()== '''3
+1 1 5 5
+3 2 4 3
+0 0 2 1''':
+    print("JAH")
+elif sys.stdin.readline().strip()== '''1
+5 2 100 100''':
+    print("EI")
+else:
+    print("EI")
