1a0d5c758eb61037ed693a084621b5bdd923b154
Pärtel <partel.kukk@gmail.com>
2024-10-28 16:49:40 +0200

    renamed

--- eio2024-lv/vandra.py
+++ eio2024-lv/vandra.py
@@ -1,32 +1,39 @@
 import sys
-n = int(sys.stdin.readline().strip())
+boards_count = int(sys.stdin.readline().strip())
+boards = []
 
-map=[]
-hasExited = False
 
-def checkKattuvus(x1,x2,y1,y2):
-    for board in map:
-        if (x1<board[0] and board[0]<x2) or (x1<board[1] and board[1]<x2):
-            #print((x1<=board[0] and board[0]<=x2))
-            #print((x1<=board[1] and board[1]<=x2))
-            #print("x")
-            if (y1<board[2] and board[2]<y2) or (y1<board[3] and board[3]<y2):
-                #print((y1<=board[2] and board[2]<=y2))
-                #print((y1<=board[3] and board[3]<=y2))
-                #print("y")
-                #print("JAH")
-                hasExited = True
-                sys.exit()
+for i in range (boards_count):
+    # ! sorting broke because they were sorted as strings not int
+    board = [int(axes) for axes in sys.stdin.readline().strip().split(" ")]
+    boards.append(board)
 
-for i in range(n):
-    #print(i)
-    plaat = sys.stdin.readline().strip().split()
-    x1 = int(plaat[0])
-    x2 = int(plaat[2])
-    y1 = int(plaat[1])
-    y2 = int(plaat[3])
-    checkKattuvus(x1,x2,y1,y2)
-    map.append([x1,x2,y1,y2])
+#sorting bc sorting would take less time than combing thru all of the boards multiple times (probably)
+boards.sort()
 
-if (hasExited != True):
-    print("EI")
+for i in range(boards_count):
+    pass
+    distance = i
+    current_board = boards[i]
+    while True:
+        distance+=1
+        if distance>boards_count-1:#count starts with 1 distance with 0
+            break
+
+        boards_for_eval = boards[distance]
+
+        #x axes check
+        if current_board[2]<=boards_for_eval[0]:
+            break #if boards are already too far apart (on the x axes)
+
+        # y axes check 
+        # *! checking logic was flawed, did not check for all occasions 
+        if (current_board[1] <  boards_for_eval[3] <=  current_board[3] or # 2.boards bottom is between
+            current_board[1] <= boards_for_eval[1] <  current_board[3] or # 2.boards top    is between
+            (current_board[1] >= boards_for_eval[1] and current_board[3] <= boards_for_eval[3])
+            ):
+            print("JAH")
+            sys.exit()
+        
+        
+print("EI")
